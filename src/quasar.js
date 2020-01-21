14cfbf80aa5130f2f0998718bfadf8e7d47c5770
Vignesh Selvaraj <s.vignesh07@gmail.com>
2020-01-22 02:08:26 +0530

    Initial commit - Vue + Quasar

---
+++ src/quasar.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+
+import './styles/quasar.scss'
+import 'quasar/dist/quasar.ie.polyfills'
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import { Quasar } from 'quasar'
+
+Vue.use(Quasar, {
+  config: {},
+  components: { /* not needed if importStrategy is not 'manual' */ },
+  directives: { /* not needed if importStrategy is not 'manual' */ },
+  plugins: {
+  }
+ })
